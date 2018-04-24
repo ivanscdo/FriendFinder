ef9689ad4b9701b83a0696f78b027ebca8dfbb84
Ivan Saucedo <ivan.scdo@gmail.com>
2018-04-24 00:43:01 -0500

    mb t424.00:42 v1.0.0 test success: server.js & htmlRoutes.js

--- server.js
+++ server.js
@@ -6,34 +6,14 @@ const   express    = require("express"),
 const   app  = express(),
         PORT = 3000;
 
-// console.log(htmlRoutes.test);
-// app.get(htmlRoutes.test);
-// app.get("/", function(req, res) {
-//         res.send("red wine, success!");
-// })
-// console.log(htmlRoutes.test);
-// htmlRoutes.test;
 
-// app.get(htmlRoutes.test);
-
-// console.log(htmlRoutes.home);
-// app.get(htmlRoutes.home);
-// console.log(htmlRoutes.home);
-
-
-
-// app.get("/*", htmlRoutes.home);
+// // // module.exports object
+// // // exports shorthand
 app.get("/", htmlRoutes.home);
-// app.get(/survey/, htmlRoutes.survey);
 app.get("/survey", htmlRoutes.survey);
 app.get("*", htmlRoutes.home);
 
-
-
-// for use with router
-// test 1
-// app.use("/",  htmlRoutes);
-// test 2
+// // // express.Router();
 // app.use(htmlRoutes);
 
 
