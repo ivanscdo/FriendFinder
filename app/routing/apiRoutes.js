457db5241db706dc17805e70f1858cc40b28d083
Ivan Saucedo <ivan.scdo@gmail.com>
2018-04-27 20:11:13 -0500

    dev f427.20:10 v2.2 found name of most compatible friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,6 +20,8 @@ router.post("/api/friends", (req, res) => {
     var totalDifference = 0;
     var totalDifference_Arr = [];
     var leastDifference = 0;
+    var mostCompatible_indexOf = 0;
+    
 
     // friends.push(newFriend);
     // console.log(newFriend);
@@ -27,99 +29,123 @@ router.post("/api/friends", (req, res) => {
     // TEST: entire array of objects
     // console.log(allFriends)
 
-    for (let x = 0; x < otherFriends_Length; x++ ) {
-        var totalDifference = 0;
-        
-        // console.log("---allFriends---");
+    function mostCompatible () {
 
-        // TEST: display name of friend
-        console.log(allFriends[x].name)
-        
-        for(let y = 0; y < 10; y++) {
-        
-        // TEST: logs all scores, alternates between newFriend & allFriends, broken up by index
-        // console.log(`---allFriends${y}---`);
-        // console.log(allFriends[x].scores[y]);
-        // console.log(`---newFriend${y}---`);                      
-        // console.log(newFriend.scores[y]);
+        for (let x = 0; x < otherFriends_Length; x++ ) {
+            var totalDifference = 0;
+            
+            // console.log("---allFriends---");
 
-        // otherFriend_Score = parseInt(allFriends[x].scores[y]);
-        otherFriend_Score = allFriends[x].scores[y];        
-        newFriend_Score = parseInt(newFriend.scores[y]);
+            // TEST: display name of friend
+            // console.log(allFriends[x].name)
+            
+            for(let y = 0; y < 10; y++) {
+            
+            // TEST: logs all scores, alternates between newFriend & allFriends, broken up by index
+            // console.log(`---allFriends${y}---`);
+            // console.log(allFriends[x].scores[y]);
+            // console.log(`---newFriend${y}---`);                      
+            // console.log(newFriend.scores[y]);
 
-        // TEST: logs difference between newFriend & allFriends scores
-        // console.log(`---difference---`);
-        // console.log(Math.abs(otherFriend_Score-newFriend_Score));
-        // console.log(scoreDifference);     
+            // otherFriend_Score = parseInt(allFriends[x].scores[y]);
+            otherFriend_Score = allFriends[x].scores[y];        
+            newFriend_Score = parseInt(newFriend.scores[y]);
 
-
-        // scoreDifference = Math.abs(parseInt(otherFriend_Score-newFriend_Score));
-        // totalDifference += parseInt(scoreDifference);
-        scoreDifference = Math.abs(otherFriend_Score-newFriend_Score);
-        totalDifference += scoreDifference;
+            // TEST: logs difference between newFriend & allFriends scores
+            // console.log(`---difference---`);
+            // console.log(Math.abs(otherFriend_Score-newFriend_Score));
+            // console.log(scoreDifference);     
 
 
-        // console.log(totalDifference);
-        // console.log(`${otherFriend_Score} -  ${newFriend_Score} =    ${scoreDifference}     || ${totalDifference}`);
+            // scoreDifference = Math.abs(parseInt(otherFriend_Score-newFriend_Score));
+            // totalDifference += parseInt(scoreDifference);
+            scoreDifference = Math.abs(otherFriend_Score-newFriend_Score);
+            totalDifference += scoreDifference;
 
-        // END OF: for(let y = 0; y < 10; y++) {
+
+            // console.log(totalDifference);
+            // console.log(`${otherFriend_Score} -  ${newFriend_Score} =    ${scoreDifference}     || ${totalDifference}`);
+
+            // END OF: for(let y = 0; y < 10; y++) {
+            }
+            // console.log("---");                
+            // totalDifference_Arr.push(parseInt(totalDifference));
+            totalDifference_Arr.push(totalDifference);
+            
+            // console.log(totalDifference_Arr);
+
+            // console.log(Math.min(parseInt(totalDifference_Arr)));
+            // leastDifference = parseInt(Math.min(totalDifference_Arr));
+            leastDifference = Math.min(...totalDifference_Arr);        
+            // console.log(leastDifference);
+
+
+
+            // TEST: log index of smallest number (most compatible friend) in totalDiff_Arr
+            // console.log(totalDifference_Arr.indexOf(Math.min(totalDifference_Arr)));
+            // console.log("index of the least difference: ", totalDifference_Arr.indexOf(leastDifference));
+
+            // TESTLOG: totaldif_arr, leastdiff, indexOf leastdiff
+            // console.log(totalDifference_Arr, leastDifference, totalDifference_Arr.indexOf(leastDifference));
+
+            // mostCompatibleFriend_index = totalDifference_Arr.indexOf(leastDifference);
+            // console.log(mostCompatibleFriend_index);
+            // return mostCompatibleFriend_index;
+            mostCompatible = totalDifference_Arr.indexOf(leastDifference);
+            
+
+            
+            
+            
+            // console.log("---");
+            // console.log("---newFriend---");                
+            // for(let y = 0; y < 10; y++) {
+            //     // console.log(allFriends[x].scores[y]);
+            //     console.log(newFriend.scores[y]);
+            // }  
+
+            // console.log("allFriends[i].scores:", allFriends[i].scores);
+            // console.log("newFriend.scores: ", newFriend.scores);
+            
+            // console.log("---totalDifference_Arr & leastDifference---");
+            // console.log(`${totalDifference_Arr} || ${leastDifference}`);
+
+
+
+            // TEST: typeof
+            // console.log("otherFriend_Score(parseInt): ", otherFriend_Score, typeof otherFriend_Score);
+            // console.log("otherFriend_Score: ",allFriends[x].scores[y], typeof allFriends[x].scores[y]);
+            // console.log("newFriend_Score(parseInt): ", newFriend_Score, typeof newFriend_Score);
+            // console.log("newFriend_Score: ", newFriend.scores[y], typeof newFriend.scores[y]);
+
+            // console.log("scoreDifference: ", scoreDifference, typeof scoreDifference);
+            // console.log("totalDifference: ", totalDifference, typeof totalDifference);
+
+            // console.log("totalDifference_Arr: ",  totalDifference_Arr, typeof totalDifference_Arr);
+            // console.log("leastDifference: ", leastDifference, typeof leastDifference);
+            // console.log("totalDifference_Arr[x]: ", totalDifference_Arr[x], typeof totalDifference_Arr[x]);       
+            // console.log("-");
+
+
+            // for (let i = 0; i < otherFriends_Length; i++) {
+
+            
+
+            // }
+
+        // END OF: for (let x = 0; x < otherFriends_Length; x++ ) {
         }
-        // console.log("---");                
-        // totalDifference_Arr.push(parseInt(totalDifference));
-        totalDifference_Arr.push(totalDifference);
-        
-        // console.log(totalDifference_Arr);
-
-        // console.log(Math.min(parseInt(totalDifference_Arr)));
-        // leastDifference = parseInt(Math.min(totalDifference_Arr));
-        leastDifference = Math.min(...totalDifference_Arr);        
-        // console.log(leastDifference);
-
-
-
-        // TEST: log index of smallest number (most compatible friend) in totalDiff_Arr
-        // console.log(totalDifference_Arr.indexOf(Math.min(totalDifference_Arr)));
-        // console.log("index of the least difference: ", totalDifference_Arr.indexOf(leastDifference));
-
-        // TESTLOG: totaldif_arr, leastdiff, indexOf leastdiff
-        // console.log(totalDifference_Arr, leastDifference, totalDifference_Arr.indexOf(leastDifference));
-
-        mostCompatibleFriend_index = totalDifference_Arr.indexOf(leastDifference);
-
-        // console.log(mostCompatibleFriend_index);
-        
-        
-        // console.log("---");
-        // console.log("---newFriend---");                
-        // for(let y = 0; y < 10; y++) {
-        //     // console.log(allFriends[x].scores[y]);
-        //     console.log(newFriend.scores[y]);
-        // }  
-
-        // console.log("allFriends[i].scores:", allFriends[i].scores);
-        // console.log("newFriend.scores: ", newFriend.scores);
-        
-        // console.log("---totalDifference_Arr & leastDifference---");
-        // console.log(`${totalDifference_Arr} || ${leastDifference}`);
-
-
-
-        // TEST: typeof
-        // console.log("otherFriend_Score(parseInt): ", otherFriend_Score, typeof otherFriend_Score);
-        // console.log("otherFriend_Score: ",allFriends[x].scores[y], typeof allFriends[x].scores[y]);
-        // console.log("newFriend_Score(parseInt): ", newFriend_Score, typeof newFriend_Score);
-        // console.log("newFriend_Score: ", newFriend.scores[y], typeof newFriend.scores[y]);
-
-        // console.log("scoreDifference: ", scoreDifference, typeof scoreDifference);
-        // console.log("totalDifference: ", totalDifference, typeof totalDifference);
-
-        // console.log("totalDifference_Arr: ",  totalDifference_Arr, typeof totalDifference_Arr);
-        // console.log("leastDifference: ", leastDifference, typeof leastDifference);
-        // console.log("totalDifference_Arr[x]: ", totalDifference_Arr[x], typeof totalDifference_Arr[x]);       
-        // console.log("-");
-
-    // END OF: for (let x = 0; x < otherFriends_Length; x++ ) {
+        return mostCompatible;
+    // END OF: function mostCompatible () {
     }
+    
+
+    // console.log(mostCompatible());
+    // mostCompatible_index = mostCompatible();
+    // console.log(mostCompatible_index);
+    console.log(allFriends[mostCompatible()].name);
+
+
 
     res.send("Questions Answered! Finding Friends")
 })
